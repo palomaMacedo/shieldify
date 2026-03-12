9e3c6012c47f82966af4cbdd4ef5d17c1577fdfc
palomaMacedo <macedo.paloma@gmail.com>
2026-03-12 16:58:12 -0300

    ✨ feat: add create account route with prisma and bcryptjs

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
+import{hash} from 'bcryptjs'
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z }from 'zod'
+import { prisma } from "@/lib/prisma";
 
 
 export async function createAccount(app: FastifyInstance){
@@ -17,10 +19,31 @@ export async function createAccount(app: FastifyInstance){
                 })
             },
         },
-        () => {
-        return 'User created!'
+       async (request, reply) => {
+            const {name, email, password} = request.body
 
-    })
+            const userWithSameEmail = await prisma.user.findUnique({
+                where: {
+                    email
+                },
+            })
 
+            if (userWithSameEmail) {
+                return reply
+                .status(400)
+                .send({ error: 'User with this email already exists' })
+            }
 
+            const passwordHash = await hash(password, 6)
+            await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    passwordHash,
+                },
+            })
+          
+            return reply.status(201).send()
+        }
+    )
 }
