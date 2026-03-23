041abce5e4c7c8cff2b62b9b651e3da8b4e670d8
palomaMacedo <macedo.paloma@gmail.com>
2026-03-23 17:57:46 -0300

    ✨ feat(frontend): add sign-in server action

--- apps/frontend/src/app/auth/sign-in/page.tsx
+++ apps/frontend/src/app/auth/sign-in/page.tsx
@@ -1,27 +1,28 @@
-import { Input } from "@/components/atoms/input"
-import { Label } from "@/components/atoms/label"
-import { Button } from "@/components/atoms/button"
-import Link from "next/link"
+  import { Input } from "@/components/atoms/input"
+  import { Label } from "@/components/atoms/label"
+  import { Button } from "@/components/atoms/button"
+  import Link from "next/link"
+  import {signInWithEmailAndPassword} from "./action"
 
-export default function SignIn() {
-  return (
-    <form action="">
-      <div className="">
-        <Label htmlFor="email">Email</Label>
-        <Input type="email" name="email" id="email" />
-      </div>
-      <div className="">
-        <Label htmlFor="password">Password</Label>
-        <Input type="password" name="password" id="password" />
-      </div>
-      <Button variant="link" asChild>
-        <Link href="/auth/forgot-password">Forgot your password?</Link>
-      </Button>
-      <Button type="submit">Sign in</Button>
-      <Button variant="link" asChild>
-        <Link href="/auth/sign-up">Don't have an account? Sign up</Link>
-      </Button>
-      <Button type="button">Sign in with GitHub</Button>
-    </form>
-  )
-}
+  export default function SignIn() {
+    return (
+      <form action={signInWithEmailAndPassword} className="flex w-full max-w-sm flex-col gap-4">
+        <div className="">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" name="email" id="email" />
+        </div>
+        <div className="">
+          <Label htmlFor="password">Password</Label>
+          <Input type="password" name="password" id="password" />
+        </div>
+        <Button variant="link" asChild>
+          <Link href="/auth/forgot-password">Forgot your password?</Link>
+        </Button>
+        <Button type="submit">Sign in</Button>
+        <Button variant="link" asChild>
+          <Link href="/auth/sign-up">Don't have an account? Sign up</Link>
+        </Button>
+        <Button type="button">Sign in with GitHub</Button>
+      </form>
+    )
+  }
